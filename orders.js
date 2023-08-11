90a3f5517e38299c3abd2c20782121951c40bfe1
Sued de Abreu Duarte <115740288+suedduarte@users.noreply.github.com>
2023-08-11 18:51:55 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Aprenda JavaScript',
+        productNumber: '85743',
+        paymentStatus: 'Venceu',
+        status: 'Pendente'
+    },
+    {
+        productName: 'CSS, o que é?',
+        productNumber: '97245',
+        paymentStatus: 'Devolveu',
+        status: 'Recusou'
+    },
+    {
+        productName: 'Como usar PHP',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
